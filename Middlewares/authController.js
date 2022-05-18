12f13d09cde1a3c1d6241002b7108b226d0cfc87
kushpathak <kushagrapathak0987@gmail.com>
2022-05-19 02:03:08 +0530

    fix: fixed bugs

--- Middlewares/authController.js
+++ Middlewares/authController.js
@@ -4,21 +4,18 @@ var cookies = require("cookie-parser");
 module.exports.requireAuth = async (req, res, next) => {
   const token = req.cookies["jwt"];
   if (!token) {
-    res.setHeader("Content-Type", "application/json");
-    res.write("Not Logged In");
+    res.send("Not Logged In");
   }
   // const token = req.cookies.jwt;
   if (token) {
     jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
       if (err) {
-        res.setHeader("Content-Type", "application/json");
-        res.write("Not Logged In");
+        res.send("Not Logged In");
       } else {
         next();
       }
     });
   } else {
-    res.setHeader("Content-Type", "application/json");
-    res.write("Not Logged In");
+    res.send("Not Logged In");
   }
 };
