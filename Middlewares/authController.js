bc293c02dcbabbf92fd7a0761d6e2095970c18be
kushpathak <kushagrapathak0987@gmail.com>
2022-05-19 02:12:00 +0530

    fix: fixed bugs

--- Middlewares/authController.js
+++ Middlewares/authController.js
@@ -3,11 +3,13 @@ const config = require("dotenv").config().parsed;
 var cookies = require("cookie-parser");
 module.exports.requireAuth = async (req, res, next) => {
   const token = req.cookies["jwt"];
+  console.log(token);
   if (!token) {
     res.send("Not Logged In");
   }
   // const token = req.cookies.jwt;
   else if (token) {
+    console.log(token, process.env.JWT_SECRET);
     jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
       if (err) {
         res.send("Not Logged In");
