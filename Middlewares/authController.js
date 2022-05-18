7a1d02534f6b11f5f7c577d331a9d7a4dd5f91db
kushpathak <kushagrapathak0987@gmail.com>
2022-05-19 02:00:16 +0530

    fix: fixed bugs

--- Middlewares/authController.js
+++ Middlewares/authController.js
@@ -14,7 +14,6 @@ module.exports.requireAuth = async (req, res, next) => {
     jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
       if (err) {
         res.setHeader("Content-Type", "application/json");
-        res.status(401);
         res.write("Not Logged In");
         res.end();
       } else {
@@ -23,7 +22,6 @@ module.exports.requireAuth = async (req, res, next) => {
     });
   } else {
     res.setHeader("Content-Type", "application/json");
-    res.status(401);
     res.write("Not Logged In");
     res.end();
   }
