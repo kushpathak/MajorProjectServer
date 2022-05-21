eeda154a3207c70e8057c1a0d1d8ebc6bdf7b32c
kushpathak <kushagrapathak0987@gmail.com>
2022-05-21 13:04:56 +0530

    deployment

--- Controllers/BlogController.js
+++ Controllers/BlogController.js
@@ -6,7 +6,7 @@ const { v4: uuidv4 } = require("uuid");
 const Trending = require("../Models/trending");
 var request = require("request");
 
-const url = 'http://1254-103-170-164-44.ngrok.io'                                                     
+const url = 'http://f050-103-170-164-44.ngrok.io'                                                     
 const ErrorHandler = (error) => {
   var Error = { title: "", snippet: "" };
   Object.values(error.errors).forEach((val) => {
